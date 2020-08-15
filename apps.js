f66a878f321c6ae82dce4182efb371ab8431af8a
David Cao <dcao@ualberta.ca>
2020-08-14 20:24:04 -0600

    finalized project

--- apps.js
+++ apps.js
@@ -8,9 +8,11 @@ let gridSize = 16;
 //call createGrid
 createGrid();
 
-//change grid option
+//change grid button
 changeGrid.addEventListener("click", function () {
-  gridSize = prompt("choose");
+  gridSize = prompt(
+    "Choose grid size from 0-100. It will generate a grid that is yourNumber x yourNumber!"
+  );
   gridContainer.setAttribute(
     `style`,
     `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr)`
@@ -19,17 +21,50 @@ changeGrid.addEventListener("click", function () {
 });
 
 //function that creates grid
+
 function createGrid() {
   for (i = 0; i < gridSize * gridSize; i++) {
+    //random colour number generator
+    const randomColourGenerator1 = Math.floor(Math.random() * 256);
+    const randomColourGenerator2 = Math.floor(Math.random() * 256);
+    const randomColourGenerator3 = Math.floor(Math.random() * 256);
+
     const grid = document.createElement("div");
     grid.classList.add("grid");
-    //hover effect
+    //hover effect default
     grid.addEventListener("mouseover", function () {
-      grid.setAttribute("style", "background: blue");
+      grid.setAttribute(`style`, `background: #3E7CB1`);
     });
     gridContainer.appendChild(grid);
 
-    //change grid Size
+    //single colour button
+    const singleColor = document.querySelector("#singleColor");
+    singleColor.addEventListener("click", function () {
+      grid.addEventListener("mouseover", function () {
+        grid.setAttribute("style", "background: #3E7CB1");
+      });
+    });
+
+    //many color button
+    const manyColor = document.querySelector("#manyColor");
+    manyColor.addEventListener("click", function () {
+      grid.addEventListener("mouseover", function () {
+        grid.setAttribute(
+          "style",
+          `background: rgb(${randomColourGenerator1}, ${randomColourGenerator2}, ${randomColourGenerator3})`
+        );
+      });
+    });
+
+    //erase button
+    const erase = document.querySelector("#erase");
+    erase.addEventListener("click", function () {
+      grid.addEventListener("mouseover", function () {
+        grid.removeAttribute("style");
+      });
+    });
+
+    //change grid Size (get rid of divs first)
     changeGrid.addEventListener("click", function () {
       gridContainer.removeChild(grid);
     });
@@ -37,10 +72,8 @@ function createGrid() {
     //grid reset
     const resetButton = document.querySelector("#resetButton");
     resetButton.addEventListener("click", function () {
-      grid.removeAttribute("style", "background: blue");
+      grid.removeAttribute(`style`);
     });
     gridContainer.appendChild(grid);
   }
 }
-
-//link gridSize to css to change the fr parameters
